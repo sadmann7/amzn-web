e2f43367ae6f3598685cd05856e49baf0644116c
sadmann7 <sadmann898@gmail.com>
2022-12-31 14:27:56 +0600

    :zap: update: prisma schema

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -29,7 +29,7 @@ export const serverSchema = z.object({
  * To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 export const clientSchema = z.object({
-  // NEXT_PUBLIC_CLIENTVAR: z.string(),
+  NEXT_PUBLIC_PRODUCTS_GET: z.string().url(),
 });
 
 /**
@@ -39,5 +39,5 @@ export const clientSchema = z.object({
  * @type {{ [k in keyof z.infer<typeof clientSchema>]: z.infer<typeof clientSchema>[k] | undefined }}
  */
 export const clientEnv = {
-  // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+  NEXT_PUBLIC_PRODUCTS_GET: process.env.NEXT_PUBLIC_PRODUCTS_GET,
 };
