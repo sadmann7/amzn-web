e4cf0154d931651204df33c4a7db9273fe95d19b
sadmann7 <sadmann898@gmail.com>
2023-01-05 10:24:46 +0600

    :zap: add: products props

--- src/pages/app/products/index.tsx
+++ src/pages/app/products/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPageWithLayout } from "@/pages/_app";
+import { trpc } from "@/utils/trpc";
 import Head from "next/head";
 
 // components imports
@@ -6,13 +7,23 @@ import DefaultLayout from "@/components/layouts/DefaultLayout";
 import ProductList from "@/components/ProductList";
 
 const Products: NextPageWithLayout = () => {
+  // trpc
+  const productsQuery = trpc.products.get.useQuery(undefined, {
+    staleTime: Infinity,
+  });
+
   return (
     <>
       <Head>
         <title>Products | Amzn Store</title>
       </Head>
       <main className="min-h-screen bg-bg-gray pb-14 pt-48 md:pt-40 lg:pt-36">
-        <ProductList />
+        {productsQuery.data ? (
+          <ProductList
+            products={productsQuery.data}
+            status={productsQuery.status}
+          />
+        ) : null}
       </main>
     </>
   );
