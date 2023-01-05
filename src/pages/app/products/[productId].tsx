cda6d26a64582c291f5e3bb0b6a9588b5bec5572
sadmann7 <sadmann898@gmail.com>
2023-01-05 10:40:20 +0600

    :zap: remove: types > globals, update: [productId]

--- src/pages/app/products/[productId].tsx
+++ src/pages/app/products/[productId].tsx
@@ -1,41 +1,42 @@
-import { getProduct } from "@/utils/queryFns";
-import type { Product } from "@prisma/client";
-import { dehydrate, QueryClient, useQuery } from "@tanstack/react-query";
-import type { GetServerSideProps } from "next";
+import { trpc } from "@/utils/trpc";
 import Head from "next/head";
 import Router from "next/router";
 import type { NextPageWithLayout } from "../../_app";
 
 // components imports
 import DefaultLayout from "@/components/layouts/DefaultLayout";
+import Loader from "@/components/Loader";
 
-type ShowProductProps = {
-  product: Product;
-};
-
-const ShowProduct: NextPageWithLayout<ShowProductProps> = (props) => {
-  // tanstack/react-query
+const ShowProduct: NextPageWithLayout = () => {
+  // trpc
   const productId = Number(Router.query.productId);
-  const { data: product, status } = useQuery<Product>({
-    queryKey: ["product", productId],
-    queryFn: () => getProduct(productId),
-    initialData: props.product,
-  });
+  const productQuery = trpc.products.getOne.useQuery(productId);
+
+  if (productQuery.isLoading) return <Loader />;
+
+  if (!productQuery.data)
+    return (
+      <div className="mx-auto w-full max-w-screen-2xl px-2 sm:w-[95vw]">
+        <div className="text-center text-base text-title md:text-lg">
+          Product not found
+        </div>
+      </div>
+    );
 
   return (
     <>
       <Head>
-        <title>{product.title ?? "Product"} | Amzn Store</title>
+        <title>{productQuery.data.title ?? "Product"} | Amzn Store</title>
       </Head>
       <main className="min-h-screen pt-48 md:pt-40 lg:pt-36">
         <div className="mx-auto w-full max-w-screen-2xl px-2 sm:w-[95vw]">
-          {status === "error" ? (
+          {productQuery.isError ? (
             <div className="text-center text-base text-title md:text-lg">
               Error in fetching product
             </div>
           ) : (
             <div className="text-center text-base text-title md:text-lg">
-              {product.title}
+              {productQuery.data.title}
             </div>
           )}
         </div>
@@ -47,17 +48,3 @@ const ShowProduct: NextPageWithLayout<ShowProductProps> = (props) => {
 export default ShowProduct;
 
 ShowProduct.getLayout = (page) => <DefaultLayout>{page}</DefaultLayout>;
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const productId = Number(ctx.query.productId);
-  const queryClient = new QueryClient();
-  await queryClient.prefetchQuery({
-    queryKey: ["product", productId],
-    queryFn: () => getProduct(productId),
-  });
-  return {
-    props: {
-      dehydratedState: dehydrate(queryClient),
-    },
-  };
-};
